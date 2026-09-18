48730198d3461df449095aa5ef357a7dcba9102c
oxfordnigeriasociety <oxfordnigeriasociety@gmail.com>
2026-09-18 12:32:36 +0100

    Update membership.ts

--- sanity/schemaTypes/membership.ts
+++ sanity/schemaTypes/membership.ts
@@ -10,5 +10,17 @@ export default defineType({
       title: 'Title',
       type: 'string',
     }),
+    defineField({
+      name: 'description',
+      title: 'Registration Instructions',
+      type: 'text',
+      description: 'Type the instructions on how to register here.',
+    }),
+    defineField({
+      name: 'applicationLink',
+      title: 'Application Link',
+      type: 'url',
+      description: 'Paste the full URL to the membership application form.',
+    }),
   ],
 })
