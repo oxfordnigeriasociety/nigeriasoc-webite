75540d605c51af9a5514b5d7382b78cf47a8998a
oxfordnigeriasociety <oxfordnigeriasociety@gmail.com>
2026-09-18 12:15:03 +0100

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,16 +2,13 @@ import { eventSchema } from './event'
 import { newsletterSchema } from './newsletter'
 import { govDocumentSchema } from './govDocument'
 import { siteSettingsSchema } from './siteSettings'
-import membership from './membership'
+import { membership } from './membership'
 // ... other imports
 
-export const schemaTypes = [
-  // ... existing types
-  membership,
-]
 export const schemaTypes = [
   eventSchema,
   newsletterSchema,
   govDocumentSchema,
+  membership,
   siteSettingsSchema,
 ]
